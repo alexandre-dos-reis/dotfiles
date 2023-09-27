c61f765ac1692912b4174ed8eac48d75de3c43cf
Alexandre Dos Reis <alexandre@finishers.com>
2023-09-27 15:33:24 +0200

    neovim: added swapfile to false

--- dot_config/nvim/lua/adr/private_core/base.lua
+++ dot_config/nvim/lua/adr/private_core/base.lua
@@ -2,9 +2,9 @@ local opt = vim.opt -- for conciseness
 
 vim.cmd("autocmd!")
 
-vim.scriptencoding = 'utf-8'
-opt.encoding = 'utf-8'
-opt.fileencoding = 'utf-8'
+vim.scriptencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
 
 vim.wo.number = true
 
@@ -19,19 +19,20 @@ opt.laststatus = 2
 opt.expandtab = true
 opt.scrolloff = 10
 opt.relativenumber = true
-opt.shell = 'fish'
-opt.backupskip = { '/tmp/*', '/private/tmp/*' }
-opt.inccommand = 'split'
+opt.shell = "fish"
+opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+opt.inccommand = "split"
 opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
 opt.smarttab = true
 opt.breakindent = true
 opt.shiftwidth = 2
 opt.tabstop = 2
-opt.wrap = false         -- No Wrap lines
-opt.backspace = { 'start', 'eol', 'indent' }
-opt.path:append { '**' } -- Finding files - Search down into subfolders
-opt.wildignore:append { '*/node_modules/*' }
+opt.wrap = false -- No Wrap lines
+opt.backspace = { "start", "eol", "indent" }
+opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+opt.wildignore:append({ "*/node_modules/*" })
 opt.clipboard:append("unnamedplus")
+opt.swapfile = false
 
 -- split windows
 opt.splitright = true
