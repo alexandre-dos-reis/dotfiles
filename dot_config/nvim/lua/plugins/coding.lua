be82ba580849aa6e7369322173e2f0f8f84f99f0
Alexandre Dos Reis <alexandre@finishers.com>
2023-11-21 12:07:54 +0100

    nvim confg

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -2,4 +2,7 @@ return {
   "smjonas/inc-rename.nvim",
   cmd = "IncRename",
   config = true,
+  keys = {
+    { "<leader>rn", ":IncRename ", { desc = "Rename variable using IncRename" } },
+  },
 }
